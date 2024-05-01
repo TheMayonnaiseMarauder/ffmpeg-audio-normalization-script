df0ee45641fb4771408b699d6dca7fe723b16d45
TheMayonnaiseMarauder <168603666+TheMayonnaiseMarauder@users.noreply.github.com>
2024-04-30 20:24:32 -0400

    Update normalize.sh

--- normalize.sh
+++ normalize.sh
@@ -3,7 +3,12 @@ targetDirectory=$1
 if [ -z "$1" ]; then
   targetDirectory=$PWD
 fi
-mkdir "${targetDirectory}/normalized/"
+if [[ "${targetDirectory}" != */ ]]; then
+    targetDirectory="${targetDirectory}/"
+fi
+echo
+mkdir "${targetDirectory}normalized/"
 for file in "${targetDirectory}"/*.mp3; do
-  ffmpeg -i "$file" -af loudnorm=I=-16:LRA=11:TP=-1.5 "${file/\/audio//audio/normalized}"
+  ffmpeg -i "$file" -af loudnorm=I=-16:LRA=11:TP=-1.5 "${file/\/audio\///audio/normalized}"
 done
+
